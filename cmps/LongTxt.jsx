cdbcf066933d626030aa06861fc68575e532872d
EdenAlmighty <edengilady1@gmail.com>
2024-03-13 11:15:33 +0200

    Add LongTxt

--- cmps/LongTxt.jsx
+++ cmps/LongTxt.jsx
@@ -1,12 +1,12 @@
-const {useState} = React
+const { useState } = React
 
-export function LongTxt({txt, length = 100}){
-    const [isSeeMore,setSeeMore] = useState(false)
-    
+export function LongTxt({ txt, length = 100 }) {
+    const [isSeeMore, setSeeMore] = useState(false)
+
 
     return <section className="long-txt">
-    <span>{isSeeMore ? txt :txt.slice(0,length)}</span>
-    {!isSeeMore && <button className="see-more-btn" onClick={() => setSeeMore(prevIsSeeMore => !prevIsSeeMore)}>See more</button>}
+        <span>{isSeeMore ? txt : txt.slice(0, length)}</span>
+        {!isSeeMore && <button className="see-more-btn" onClick={() => setSeeMore(prevIsSeeMore => !prevIsSeeMore)}>See more</button>}
     </section>
 
 }
