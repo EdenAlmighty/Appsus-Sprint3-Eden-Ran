6aa66245cc6a4a874f40baa230eec74b9c6a3560
Ran Sabban <sabbanran@gmail.com>
2024-03-15 18:01:56 +0200

    Icons adj

--- apps/mail/cmps/MailFolderList.jsx
+++ apps/mail/cmps/MailFolderList.jsx
@@ -3,34 +3,34 @@ const { Link, useSearchParams } = ReactRouterDOM
 
 import { mailService } from '../services/mail.service.js'
 
-export function MailFolderList({ onSetFilter, filterBy = getFilterFromParams(searchParams), unreadCount }){
+export function MailFolderList({ onSetFilter, filterBy = getFilterFromParams(searchParams), unreadCount }) {
 
 
-    console.log(filterBy);    
+    console.log(filterBy);
 
     const [filterByToUpdate, setFilterByToUpdate] = useState(filterBy)
     useEffect(() => {
         onSetFilter(filterByToUpdate)
-    },[filterByToUpdate])
+    }, [filterByToUpdate])
 
-    function handleChange(value){
-        setFilterByToUpdate({...filterBy, ['status']: value})
+    function handleChange(value) {
+        setFilterByToUpdate({ ...filterBy, ['status']: value })
     }
 
-    function getFolderClass(value){
-        if(filterBy.status === value){
+    function getFolderClass(value) {
+        if (filterBy.status === value) {
             return 'focused'
         }
     }
 
     return <section className="mail-folder-list">
         <ul className="clean-list mail-folder-list-items">
-            <li className={`inbox ${getFolderClass('inbox')}`} onClick={() => handleChange('inbox')}>Inbox <span className="unread-count">{unreadCount}</span></li>
-            <li className={getFolderClass('sent')}  onClick={() => handleChange('sent')}>Sent</li>
-            <li className={getFolderClass('trash')}  onClick={() => handleChange('trash')}>Trash</li>
-            <li className={getFolderClass('draft')}  onClick={() => handleChange('draft')}>Draft</li>
-            <li className={getFolderClass('star')}  onClick={() => handleChange('star')}>Starred</li>
+            <li className={`inbox ${getFolderClass('inbox')}`} onClick={() => handleChange('inbox')}><span className="material-symbols-outlined">inbox</span>Inbox <span className="unread-count">{unreadCount}</span></li>
+            <li className={getFolderClass('sent')} onClick={() => handleChange('sent')}><span className="material-symbols-outlined">send</span>Sent</li>
+            <li className={getFolderClass('trash')} onClick={() => handleChange('trash')}><span className="material-symbols-outlined">delete</span>Trash</li>
+            <li className={getFolderClass('draft')} onClick={() => handleChange('draft')}><span className="material-symbols-outlined">draft</span>Draft</li>
+            <li className={getFolderClass('star')} onClick={() => handleChange('star')}><span className="material-symbols-outlined">star</span>Starred</li>
         </ul>
     </section>
-    
+
 }
--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -13,6 +13,13 @@ export function MailPreview({ mail, onSetRead, onRemoveMail, onStarMail, expande
 
     function getFormattedDate(mail) {
         const date = new Date(mail.sentAt)
+        // if (Date.now() - date > 12000) {
+        //     const formattedDate = Date.now() - date / 1000 / 60
+        //     return `${formattedDate} minutes..`
+        // }
+        // if (Date.now() - date < 12000){
+        //     return `Just now...`
+        // }
         return `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`
     }
 
@@ -29,11 +36,12 @@ export function MailPreview({ mail, onSetRead, onRemoveMail, onStarMail, expande
     // }
 
     function moveToPageDetails(mailId) {
-        console.log('clicked');
+        onSetRead(true,mailId)
         navigate(`/mail/${mailId}`)
     }
 
     function moveToPagePreview(mailId) {
+        onSetRead(true,mailId)
         isExpended ? setExpandedRowId('') : setExpandedRowId(mailId)
     }
 
@@ -47,20 +55,26 @@ export function MailPreview({ mail, onSetRead, onRemoveMail, onStarMail, expande
 
     }
 
-    
+    function getStarClass() {
+        return mail.star ? 'star-active' : ''
+    }
+
+    function getReadIcon() {
+        return mail.isRead ? <span class="material-symbols-outlined">drafts</span> : <span class="material-symbols-outlined">mark_email_unread</span>
+    }
 
     return <Fragment>
         <tr key={mail.id} className={getMailClass(mail.isRead, mail.id)} >
             <td>
-                <div className="mail-actions left" onClick={() => onStarMail(mail.id)}>{mail.star ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" /></svg> : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.7 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z" /></svg>} </div>
+                <div className={`mail-actions left`} onClick={() => onStarMail(mail.id)}><span class={`material-symbols-outlined ${getStarClass()}`}>star</span></div>
             </td>
             <td onClick={() => moveToPageDetails(mail.id)} className="list-sender">{mail.sender}</td>
             {/* <td>{mail.from}</td> */}
             <td className="list-subject" onClick={() => moveToPageDetails(mail.id)}>{getFormattedSubject(mail)}</td>
             <td onClick={() => moveToPageDetails(mail.id)} className="mail-body-msg">{mail.body}</td>
             <td className="mail-actions-right-container">
-                <button className="mail-actions right" onClick={() => onSetRead(!mail.isRead, mail.id)}>{mail.isRead ? '💌' : '📧'}</button>
-                <button className="mail-actions right" onClick={() => onRemoveMail(mail.id)}>🗑️</button>
+                <button className="mail-actions right" onClick={() => onSetRead(!mail.isRead, mail.id)}>{getReadIcon()}</button>
+                <button className="mail-actions right" onClick={() => onRemoveMail(mail.id)}><span className="material-symbols-outlined">delete</span></button>
                 <button className="mail-actions right" onClick={() => moveToPagePreview(mail.id)}>Preview</button>
             </td>
             <td className="mail-date-list" onClick={() => moveToPageDetails(mail.id)}>{getFormattedDate(mail)}</td>
@@ -76,8 +90,8 @@ export function MailPreview({ mail, onSetRead, onRemoveMail, onStarMail, expande
                     </section>
                 </td>
                 <section className="actions-buttons-extended-container">
-                    <button className="mail-actions-extended" onClick={() => onSetRead(!mail.isRead, mail.id)}>{mail.isRead ? '💌' : '📧'}</button>
-                    <button className="mail-actions-extended" onClick={() => onRemoveMail(mail.id)}>🗑️</button>
+                    <button className="mail-actions-extended" onClick={() => onSetRead(!mail.isRead, mail.id)}>{getReadIcon()}</button>
+                    <button className="mail-actions-extended" onClick={() => onRemoveMail(mail.id)}><span className="material-symbols-outlined">delete</span></button>
                     <button className="mail-actions-extended" onClick={() => moveToPageDetails(mail.id)}>Go to mail</button>
                 </section>
             </tr>
