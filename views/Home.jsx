ec4072a3676f97aa14951a5b684c5b1195afb762
EdenAlmighty <edengilady1@gmail.com>
2024-03-17 00:30:41 +0200

    Added home page animations

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,27 +1,35 @@
+import { HeroSvg } from "../cmps/HeroSvg.jsx";
+
+
 export function Home() {
     return (
         <section className="home">
             <header>
 
                 <div className="hero">
-                    <h1>Let's organize</h1>
-                    <p>Appsus is here to help you organize your life, and make it a little easier.</p>
-                    <button>Get Started</button>
-                    <div className="animated-squares">
+                    <section className="hero-title">
+                        <h2>Let's organize</h2>
+                        <p>appsus is here to help you organize your life, and make it a little easier.</p>
+                        <button className="hero-cta">Get Started</button>
+                    </section>
+                    <section className="hero-svg">
+                        <HeroSvg />
+                    </section>
+                    {/* <div className="animated-squares">
                         <div className="square" style={{ animationDelay: '0s' }}></div>
                         <div className="square" style={{ animationDelay: '1s' }}></div>
                         <div className="square" style={{ animationDelay: '2s' }}></div>
-                    </div>
+                    </div> */}
                 </div>
             </header>
 
             <section className="features">
-                <h2>Our Features</h2>
+                <h2>Our Apps</h2>
                 <div className="feature">
                     <div className="icon-check"></div>
                     <p>Appsus is here to help you organize your life, and make it a little easier.</p>
                 </div>
             </section>
         </section>
-    );
+    )
 }
